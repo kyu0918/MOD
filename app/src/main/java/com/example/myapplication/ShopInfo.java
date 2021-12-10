272d7db9cfd180a2a4d057c231942e1a74367e10
kyu0918 <58536057+kyu0918@users.noreply.github.com>
2021-12-10 10:38:43 +0900

    샵 상세 정보 페이지 수정 및 Back 버튼 활성화

--- app/src/main/java/com/example/myapplication/ShopInfo.java
+++ app/src/main/java/com/example/myapplication/ShopInfo.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class ShopInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_info);
+
+        ImageButton back = findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
